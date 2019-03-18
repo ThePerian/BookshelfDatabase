ce66113fa8e4d3acb527ad1dcdb9747dda095a05
John Doe <h.perian@gmail.com>
2019-03-18 11:26:30 +0300

    Logic for Insert command.

--- BookshelfDAL/ConnectedLayer/InventoryDAL.cs
+++ BookshelfDAL/ConnectedLayer/InventoryDAL.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Data.SqlClient;
 using System.Collections.Generic;
+using BookshelfDAL.Titles;
 
 namespace BookshelfDAL.ConnectedLayer
 {
@@ -17,6 +18,27 @@ namespace BookshelfDAL.ConnectedLayer
         public void CloseConnection()
         {
             _sqlConnection.Close();
+        }
+
+        public void InsertBook(int id, string name, string author, bool read)
+        {
+            //Сформировать строку запроса
+            string sqlString = "Insert Into Inventory" +
+                $"(Author, Name, Read) Values ('{author}', '{name}', '{read}')";
+            //Выполнить запрос
+            using (SqlCommand command = new SqlCommand(sqlString))
+                command.ExecuteNonQuery();
+        }
+
+        public void InsertBook(NewBook book)
+        {
+            //Сформировать строку запроса
+            string sqlString = "Insert Into Inventory" +
+                "(Author, Name, Read) Values" +
+                $"('{book.Author}', '{book.Name}', '{book.Read}')";
+            //Выполнить запрос
+            using (SqlCommand command = new SqlCommand(sqlString))
+                command.ExecuteNonQuery();
         }
     }
 }
---
+++ BookshelfDAL/Titles/NewBook.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BookshelfDAL.Titles
+{
+    public class NewBook
+    {
+        public int BookId { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public bool Read { get; set; }
+    }
+}
