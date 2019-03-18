5d91fa9ffdc02897bd277718db2fdbec5a928bce
John Doe <h.perian@gmail.com>
2019-03-18 11:06:49 +0300

    Beginning of library for different data access layers.

---
+++ BookshelfDAL/ConnectedLayer/InventoryDAL.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+
+namespace BookshelfDAL.ConnectedLayer
+{
+    public class InventoryDAL
+    {
+        private SqlConnection _sqlConnection = null;
+
+        public void OpenConnection(string connectionString)
+        {
+            _sqlConnection = new SqlConnection { ConnectionString = connectionString };
+            _sqlConnection.Open();
+        }
+
+        public void CloseConnection()
+        {
+            _sqlConnection.Close();
+        }
+    }
+}
