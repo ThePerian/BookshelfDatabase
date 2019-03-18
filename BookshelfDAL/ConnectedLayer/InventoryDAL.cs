9a093bbc1bebc9454f22a2ec24d2bdddd6db7f64
John Doe <h.perian@gmail.com>
2019-03-18 11:51:44 +0300

    Logic for 'Update Read' and 'Select *' commands.

--- BookshelfDAL/ConnectedLayer/InventoryDAL.cs
+++ BookshelfDAL/ConnectedLayer/InventoryDAL.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data;
 using System.Data.SqlClient;
 using System.Collections.Generic;
 using BookshelfDAL.Titles;
@@ -57,6 +58,57 @@ namespace BookshelfDAL.ConnectedLayer
                     throw error;
                 }
             }
+        }
+
+        public void UpdateBookReadStatus(int id, bool read)
+        {
+            string sqlString = $"Update Inventory Set Read = '{read}' Where BookId = '{id}'";
+            using (SqlCommand command = new SqlCommand(sqlString, _sqlConnection))
+                command.ExecuteNonQuery();
+        }
+
+        public List<NewBook> GetAllInventoryAsList()
+        {
+            //Здесь будут храниться записи
+            List<NewBook> inventory = new List<NewBook>();
+
+            //Подготовить строку запроса
+            string sqlString = "Select * from Inventory";
+            using (SqlCommand command = new SqlCommand(sqlString, _sqlConnection))
+            {
+                //Выполнить запрос и прочитать данные
+                SqlDataReader dataReader = command.ExecuteReader();
+                while (dataReader.Read())
+                {
+                    inventory.Add(new NewBook
+                    {
+                        BookId = (int)dataReader["BookId"],
+                        Name = (string)dataReader["Name"],
+                        Author = (string)dataReader["Author"],
+                        Read = (bool)dataReader["Read"]
+                    });
+                }
+                dataReader.Close();
+            }
+            return inventory;
+        }
+
+        public DataTable GetAllInventoryAsDataTable()
+        {
+            //Здесь будут храниться записи
+            DataTable inventory = new DataTable();
+
+            //Подготовить строку запроса
+            string sqlString = "Select * from Inventory";
+            using (SqlCommand command = new SqlCommand(sqlString, _sqlConnection))
+            {
+                //Выполнить запрос и прочитать данные
+                SqlDataReader dataReader = command.ExecuteReader();
+                inventory.Load(dataReader);
+                dataReader.Close();
+            }
+
+            return inventory;
         }
     }
 }
