6da8324167757c02458f642ceae2ae15afbe5eb6
John Doe <h.perian@gmail.com>
2019-03-21 17:03:07 +0300

    GUI to update database using disconnected layer.

---
+++ InventoryDALDCGUI/MainForm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using BookshelfDAL.DisconnectedLayer;
+
+namespace InventoryDALDCGUI
+{
+    public partial class MainForm : Form
+    {
+        InventoryDALDC _daldc = null;
+        public MainForm()
+        {
+            InitializeComponent();
+            string connectionString =
+                @"Data Source=(local)\SQLEXPRESS;Integrated Security=true;" +
+                "Initial Catalog=Bookshelf;Pooling=false";
+            //Создать объект доступа к данным
+            _daldc = new InventoryDALDC(connectionString);
+            //Заполнить элемент управления GridView
+            inventoryGridView.DataSource = _daldc.GetAllInventory();
+        }
+
+        private void btnUpdateInventory_Click(object sender, EventArgs e)
+        {
+            //Получить модифицированные данные из GridView
+            DataTable modifiedTable = (DataTable)inventoryGridView.DataSource;
+
+            try
+            {
+                //Зафиксировать изменения
+                _daldc.UpdateInventory(modifiedTable);
+                inventoryGridView.DataSource = _daldc.GetAllInventory();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
