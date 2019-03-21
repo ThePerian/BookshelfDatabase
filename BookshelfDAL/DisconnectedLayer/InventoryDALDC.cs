8d7b62f331feb2e2783a0279b21e594c09dd359b
John Doe <h.perian@gmail.com>
2019-03-21 16:48:25 +0300

    Add library to use disconnected layer.

---
+++ BookshelfDAL/DisconnectedLayer/InventoryDALDC.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace BookshelfDAL.DisconnectedLayer
+{
+    public class InventoryDALDC
+    {
+        private string _connectionString;
+        private SqlDataAdapter _adapter = null;
+
+        public InventoryDALDC(string connectionString)
+        {
+            _connectionString = connectionString;
+            ConfigureAdapter(out _adapter);
+        }
+
+        private void ConfigureAdapter(out SqlDataAdapter adapter)
+        {
+            //Создать адаптер и настроить SelectCommand
+            adapter = new SqlDataAdapter("Select * from Inventory", _connectionString);
+
+            //Динамически получить остальные объекты команд во время выполнения
+            var builder = new SqlCommandBuilder(adapter);
+        }
+
+        public DataTable GetAllInventory()
+        {
+            DataTable inventory = new DataTable("Inventory");
+            //Получить все записи в таблице Inventory
+            _adapter.Fill(inventory);
+            return inventory;
+        }
+
+        public void UpdateInventory(DataTable modifiedTable)
+        {
+            //Проверить RowState у каждой строки таблицы и при необходимости внести изменения
+            _adapter.Update(modifiedTable);
+        }
+
+        
+    }
+}
