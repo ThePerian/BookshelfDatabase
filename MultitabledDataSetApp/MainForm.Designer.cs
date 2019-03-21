9d4c87f0fba315dcbd17ee8b8fac7a9614717a85
John Doe <h.perian@gmail.com>
2019-03-21 17:59:34 +0300

    Beginning of test project to manipulate different tables at once.

---
+++ MultitabledDataSetApp/MainForm.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace MultitabledDataSetApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblInventory = new System.Windows.Forms.Label();
+            this.inventoryGridView = new System.Windows.Forms.DataGridView();
+            this.lblStores = new System.Windows.Forms.Label();
+            this.storesGridView = new System.Windows.Forms.DataGridView();
+            this.lblOrders = new System.Windows.Forms.Label();
+            this.ordersGridView = new System.Windows.Forms.DataGridView();
+            this.btnUpdateDatabase = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.inventoryGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.storesGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ordersGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblInventory
+            // 
+            this.lblInventory.AutoSize = true;
+            this.lblInventory.Location = new System.Drawing.Point(12, 9);
+            this.lblInventory.Name = "lblInventory";
+            this.lblInventory.Size = new System.Drawing.Size(119, 13);
+            this.lblInventory.TabIndex = 0;
+            this.lblInventory.Text = "Текущее содержимое";
+            // 
+            // inventoryGridView
+            // 
+            this.inventoryGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.inventoryGridView.Location = new System.Drawing.Point(12, 25);
+            this.inventoryGridView.Name = "inventoryGridView";
+            this.inventoryGridView.Size = new System.Drawing.Size(539, 150);
+            this.inventoryGridView.TabIndex = 1;
+            // 
+            // lblStores
+            // 
+            this.lblStores.AutoSize = true;
+            this.lblStores.Location = new System.Drawing.Point(9, 178);
+            this.lblStores.Name = "lblStores";
+            this.lblStores.Size = new System.Drawing.Size(59, 13);
+            this.lblStores.TabIndex = 2;
+            this.lblStores.Text = "Магазины";
+            // 
+            // storesGridView
+            // 
+            this.storesGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.storesGridView.Location = new System.Drawing.Point(12, 194);
+            this.storesGridView.Name = "storesGridView";
+            this.storesGridView.Size = new System.Drawing.Size(539, 150);
+            this.storesGridView.TabIndex = 3;
+            // 
+            // lblOrders
+            // 
+            this.lblOrders.AutoSize = true;
+            this.lblOrders.Location = new System.Drawing.Point(12, 347);
+            this.lblOrders.Name = "lblOrders";
+            this.lblOrders.Size = new System.Drawing.Size(46, 13);
+            this.lblOrders.TabIndex = 4;
+            this.lblOrders.Text = "Заказы";
+            // 
+            // ordersGridView
+            // 
+            this.ordersGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ordersGridView.Location = new System.Drawing.Point(12, 363);
+            this.ordersGridView.Name = "ordersGridView";
+            this.ordersGridView.Size = new System.Drawing.Size(539, 150);
+            this.ordersGridView.TabIndex = 5;
+            // 
+            // btnUpdateDatabase
+            // 
+            this.btnUpdateDatabase.Location = new System.Drawing.Point(390, 519);
+            this.btnUpdateDatabase.Name = "btnUpdateDatabase";
+            this.btnUpdateDatabase.Size = new System.Drawing.Size(161, 23);
+            this.btnUpdateDatabase.TabIndex = 6;
+            this.btnUpdateDatabase.Text = "Обновить базу данных";
+            this.btnUpdateDatabase.UseVisualStyleBackColor = true;
+            this.btnUpdateDatabase.Click += new System.EventHandler(this.btnUpdateDatabase_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(563, 548);
+            this.Controls.Add(this.btnUpdateDatabase);
+            this.Controls.Add(this.ordersGridView);
+            this.Controls.Add(this.lblOrders);
+            this.Controls.Add(this.storesGridView);
+            this.Controls.Add(this.lblStores);
+            this.Controls.Add(this.inventoryGridView);
+            this.Controls.Add(this.lblInventory);
+            this.Name = "MainForm";
+            this.Text = "База данных Bookshelf";
+            ((System.ComponentModel.ISupportInitialize)(this.inventoryGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.storesGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ordersGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblInventory;
+        private System.Windows.Forms.DataGridView inventoryGridView;
+        private System.Windows.Forms.Label lblStores;
+        private System.Windows.Forms.DataGridView storesGridView;
+        private System.Windows.Forms.Label lblOrders;
+        private System.Windows.Forms.DataGridView ordersGridView;
+        private System.Windows.Forms.Button btnUpdateDatabase;
+    }
+}
+
---
+++ MultitabledDataSetApp/MainForm.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using System.Configuration;
+
+namespace MultitabledDataSetApp
+{
+    public partial class MainForm : Form
+    {
+        private DataSet _bookshelfDataSet = new DataSet("Bookshelf");
+
+        private SqlCommandBuilder _sqlCbInventory;
+        private SqlCommandBuilder _sqlCbStores;
+        private SqlCommandBuilder _sqlCbOrders;
+
+        private SqlDataAdapter _inventoryAdapter;
+        private SqlDataAdapter _storesAdapter;
+        private SqlDataAdapter _ordersAdapter;
+
+        private string _connectionString;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            //Получить строку подключения
+            _connectionString =
+                ConfigurationManager.ConnectionStrings["BookshelfSqlProvider"].ConnectionString;
+            //Создать объекты адаптеров
+            _inventoryAdapter = new SqlDataAdapter("Select * from Inventory", _connectionString);
+            _storesAdapter = new SqlDataAdapter("Select * from Stores", _connectionString);
+            _ordersAdapter = new SqlDataAdapter("Select * from Orders", _connectionString);
+            //Автоматически сгенерировать команды
+            _sqlCbInventory = new SqlCommandBuilder(_inventoryAdapter);
+            _sqlCbOrders = new SqlCommandBuilder(_ordersAdapter);
+            _sqlCbStores = new SqlCommandBuilder(_storesAdapter);
+            //Заполнить таблицы
+            _inventoryAdapter.Fill(_bookshelfDataSet, "Inventory");
+            _storesAdapter.Fill(_bookshelfDataSet, "Stores");
+            _ordersAdapter.Fill(_bookshelfDataSet, "Orders");
+            //Построить отношения между таблицами
+            BuildTableRelationship();
+            //Привязаться к сеткам
+            inventoryGridView.DataSource = _bookshelfDataSet.Tables["Inventory"];
+            storesGridView.DataSource = _bookshelfDataSet.Tables["Stores"];
+            ordersGridView.DataSource = _bookshelfDataSet.Tables["Orders"];
+        }
+
+        private void BuildTableRelationship()
+        {
+            //Создать объект отношения между данными StoreOrder
+            DataRelation dataRelation = new DataRelation("StoreOrder",
+                _bookshelfDataSet.Tables["Stores"].Columns["StoreId"],
+                _bookshelfDataSet.Tables["Orders"].Columns["StoreId"]);
+            _bookshelfDataSet.Relations.Add(dataRelation);
+
+            //Создать объект отношения между данными InventoryOrder
+            dataRelation = new DataRelation("InventoryOrder",
+                _bookshelfDataSet.Tables["Inventory"].Columns["BookId"],
+                _bookshelfDataSet.Tables["Orders"].Columns["BookId"]);
+            _bookshelfDataSet.Relations.Add(dataRelation);
+        }
+
+        private void btnUpdateDatabase_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                _inventoryAdapter.Update(_bookshelfDataSet, "Inventory");
+                _storesAdapter.Update(_bookshelfDataSet, "Stores");
+                _ordersAdapter.Update(_bookshelfDataSet, "Orders");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
