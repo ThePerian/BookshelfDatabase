78a4748535b3ff43dab4cf63d8d1b3f423996b44
John Doe <h.perian@gmail.com>
2019-03-22 14:42:04 +0300

    Example of console client to use with auto-generated classes.

---
+++ StronglyTypedDataSetConsoleClient/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using BookshelfDAL.DataSets;
+using BookshelfDAL.DataSets.BookshelfDataSetTableAdapters;
+using static System.Console;
+using System.Data;
+
+namespace StronglyTypedDataSetConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("*** Использование строго типизированных DataSet ***\n");
+
+            var table = new BookshelfDataSet.InventoryDataTable();
+            var adapter = new InventoryTableAdapter();
+
+            adapter.Fill(table);
+            PrintInventory(table);
+            ReadKey();
+        }
+
+        static void PrintInventory(BookshelfDataSet.InventoryDataTable table)
+        {
+            for (int curCol = 0; curCol < table.Columns.Count; curCol++)
+                Write(table.Columns[curCol].ColumnName + "\t");
+            WriteLine();
+
+            for (int curRow = 0; curRow < table.Rows.Count; curRow++)
+            {
+                for (int curCol = 0; curCol < table.Columns.Count; curCol++)
+                    Write(table.Rows[curRow][curCol] + "\t");
+                WriteLine();
+            }
+        }
+    }
+}
