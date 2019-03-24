67ec4d1c45a2ef51b381b226370925732d90ac78
John Doe <h.perian@gmail.com>
2019-03-24 15:47:49 +0300

    Try adding and removing records.

--- BookshelfConsoleApp/Program.cs
+++ BookshelfConsoleApp/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using BookshelfConsoleApp.EF;
+using static System.Console;
 
 namespace BookshelfConsoleApp
 {
@@ -10,6 +8,72 @@ namespace BookshelfConsoleApp
     {
         static void Main(string[] args)
         {
+            WriteLine("***Using Entity Framework***\n");
+
+            int bookId = AddNewRecord();
+
+            if (bookId < 0)
+                WriteLine("Не удалось добавить запись");
+            else
+                WriteLine($"Создана запись {bookId}");
+            PrintAllInventory();
+
+            RemoveRecord(bookId);
+            WriteLine($"Удалена запись {bookId}");
+            PrintAllInventory();
+
+            ReadKey();
+        }
+
+        private static int AddNewRecord()
+        {
+            using (var context = new BookshelfEntities())
+            {
+                try
+                {
+                    var book = new Book()
+                    {
+                        Author = "Брэндон Сандэрсон",
+                        BookName = "Обреченное королевство",
+                        ReadStatus = false
+                    };
+                    context.Inventory.Add(book);
+                    context.SaveChanges();
+
+                    return book.BookId;
+                }
+                catch(Exception ex)
+                {
+                    WriteLine(ex.InnerException.Message);
+                    return -1;
+                }
+            }
+        }
+
+        private static void RemoveRecord(int bookId)
+        {
+            using (var context = new BookshelfEntities())
+            {
+                try
+                {
+                    var bookToRemove = context.Inventory.Find(bookId);
+                    context.Inventory.Remove(bookToRemove);
+                    context.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    WriteLine(ex.Message);
+                }
+            }
+        }
+
+        private static void PrintAllInventory()
+        {
+            using (var context = new BookshelfEntities())
+            {
+                foreach(Book item in context.Inventory)
+                    WriteLine(item);
+            }
         }
     }
 }
