bb3489b20f7a49fd50384fc5f385be973b283846
John Doe <h.perian@gmail.com>
2019-03-26 17:34:04 +0300

    Add inventory repository.

---
+++ BookshelfDALEF/Repos/InventoryRepo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Data.Entity;
+using System.Threading.Tasks;
+using BookshelfDALEF.Models;
+
+namespace BookshelfDALEF.Repos
+{
+    class InventoryRepo : BaseRepo<Inventory>, IRepo<Inventory>
+    {
+        public InventoryRepo()
+        {
+            Table = Context.Inventory;
+        }
+
+        public int Delete(int id)
+        {
+            Context.Entry(new Inventory() { BookId = id }).State = EntityState.Deleted;
+            return SaveChanges();
+        }
+
+        public Task<int> DeleteAsync(int id)
+        {
+            Context.Entry(new Inventory() { BookId = id }).State = EntityState.Deleted;
+            return SaveChangesAsync();
+        }
+    }
+}
