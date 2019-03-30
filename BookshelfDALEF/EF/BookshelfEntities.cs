b881bdb4177c1df1f934503c2af982bf08d414aa
John Doe <h.perian@gmail.com>
2019-03-30 15:17:07 +0300

    Add event observers as alternative to interceptors.

--- BookshelfDALEF/EF/BookshelfEntities.cs
+++ BookshelfDALEF/EF/BookshelfEntities.cs
@@ -2,17 +2,26 @@ namespace BookshelfDALEF.EF
 {
     using System;
     using System.Data.Entity;
+    using System.Data.Entity.Infrastructure;
     using System.Data.Entity.Infrastructure.Interception;
     using BookshelfDALEF.Models;
 
     public class BookshelfEntities : DbContext
     {
+        //Создать логгер
         static readonly DatabaseLogger DbLogger = new DatabaseLogger("F:\\sqllog.txt", true);
 
         public BookshelfEntities() : base("name=BookshelfConnection")
         {
+            //Добавить логгер в список перехватчиков и начать запись
             DbLogger.StartLogging();
             DbInterception.Add(DbLogger);
+            //Альтернатива перехватчику - отслеживание событий
+            var context = (this as IObjectContextAdapter).ObjectContext;
+            //Происходит, когда объект реконструируется из хранилища данных
+            context.ObjectMaterialized += OnObjectMaterialized;
+            //Происходит, когда данные объекта распространяются в хранилище данных
+            context.SavingChanges += OnSavingChanges;
         }
 
         protected override void Dispose(bool disposing)
@@ -20,6 +29,17 @@ namespace BookshelfDALEF.EF
             DbInterception.Remove(DbLogger);
             DbLogger.StopLogging();
             base.Dispose(disposing);
+        }
+
+        private void OnSavingChanges(object sender, EventArgs eventArgs)
+        {
+
+        }
+
+        private void OnObjectMaterialized(object sender, 
+            System.Data.Entity.Core.Objects.ObjectMaterializedEventArgs eventArgs)
+        {
+
         }
 
         public virtual DbSet<Wishlist> Wishlist { get; set; }
