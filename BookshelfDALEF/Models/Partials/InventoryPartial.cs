800244a1ee375239f0d97e5c3a0a9beea01e9448
John Doe <h.perian@gmail.com>
2019-03-30 16:48:00 +0300

    Better conversion to string for Inventory object.

--- BookshelfDALEF/Models/Partials/InventoryPartial.cs
+++ BookshelfDALEF/Models/Partials/InventoryPartial.cs
@@ -8,13 +8,14 @@ namespace BookshelfDALEF.Models
     public partial class Inventory
     {
         [NotMapped]
-        public string AuthorBookName => $"{Author} + ({BookName})";
+        public string AuthorBookName => $"{Author} + \"{BookName}\"";
 
         public override string ToString()
         {
+            string readStatus = ReadStatus ? "прочитано" : "не прочитано";
             //Поскольку автор или название могут быть не указаны, предоставить стандартные
             return $"{BookId}: {Author??"Без автора"} " +
-                $"\"{BookName??"Без названия"}\", прочитано: {ReadStatus.ToString()}";
+                $"\"{BookName??"Без названия"}\", {ReadStatus}";
         }
     }
 }
