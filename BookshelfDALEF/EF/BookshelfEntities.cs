3cb7f9c6be118075340ae497924b2c70ee9c9e3b
John Doe <h.perian@gmail.com>
2019-03-30 14:48:21 +0300

    Add database logger.

--- BookshelfDALEF/EF/BookshelfEntities.cs
+++ BookshelfDALEF/EF/BookshelfEntities.cs
@@ -2,13 +2,24 @@ namespace BookshelfDALEF.EF
 {
     using System;
     using System.Data.Entity;
+    using System.Data.Entity.Infrastructure.Interception;
     using BookshelfDALEF.Models;
 
     public class BookshelfEntities : DbContext
     {
-        public BookshelfEntities()
-            : base("name=BookshelfConnection")
+        static readonly DatabaseLogger DbLogger = new DatabaseLogger("F:\\sqllog.txt", true);
+
+        public BookshelfEntities() : base("name=BookshelfConnection")
         {
+            DbLogger.StartLogging();
+            DbInterception.Add(DbLogger);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            DbInterception.Remove(DbLogger);
+            DbLogger.StopLogging();
+            base.Dispose(disposing);
         }
 
         public virtual DbSet<Wishlist> Wishlist { get; set; }
